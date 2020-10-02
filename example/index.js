ade4afd4d3b4151c529a36bc1bb532beff1e1856
Kushal Garg <kushalgarg2000@gmail.com>
2020-10-02 20:22:32 +0530

    Adds functions for providing raw code as argument

--- example/index.js
+++ example/index.js
@@ -1,21 +1,23 @@
-const MossClient = require('../moss')
-
-const userId = process.env.MOSS_ID ? process.env.MOSS_ID : 'YOUR_USER_ID_HERE'
+const MossClient = require("../moss")
+const fs = require("fs")
+const userId = process.env.MOSS_ID ? process.env.MOSS_ID : "YOUR_USER_ID_HERE"
 
 let client = new MossClient("python", userId)
 
 async function start() {
-    try {
-        client.setComment("project1")
-        client.addBaseFile('./submissions/base.py', 'base')
-        client.addFile('./submissions/sub1.py', 'sub1')
-        client.addFile('./submissions/sub2.py', 'sub2')
+	try {
+		const sub2 = fs.readFileSync("./submissions/sub2.py").toString()
 
-        let url = await client.process()
-        console.log(url)
-    } catch (e) {
-        console.log("Error with MOSS submission")
-    }
+		client.setComment("project1")
+		client.addBaseFile("./submissions/base.py", "base")
+		client.addFile("./submissions/sub1.py", "sub1")
+		client.addRawFile(sub2, "sub2")
+
+		let url = await client.process()
+		console.log(url)
+	} catch (e) {
+		console.log("Error with MOSS submission")
+	}
 }
 
 start()
